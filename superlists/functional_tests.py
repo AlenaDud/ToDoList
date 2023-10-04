ab3eaf49fbdbd8552e311026f0a1ec07c5656e27
Alyona Dudina <badyukiko@gmail.com>
2023-10-04 09:32:28 +0300

    create FT for browsers title

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,46 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'install' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """Testing new visitor."""
 
+    def setUp(self) -> None:
+        """Setup firefox browser."""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        """Exit from browser."""
+        self.browser.quit()
+
+    def test_can_start_a_list_retrieve_it_later(self):
+        """Testing start list and get them later."""
+        self.browser.get('http://localhost:8000')
+
+        # Makes sure by looking at the title that
+        # it is definitely on the to-do page
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End test')
+
+
+    # Tries to add 'buy the book' to the to-do list
+
+    # Press 'Enter' to see the result
+
+    # Sees the updated list witch contain '1. buy the book'
+
+    # Text area prompts you to enter another item
+    # User enters 'read this book' and press 'Enter'
+
+    # Page is being updated and now containing both items
+
+    # The user checks whether the site remembers her affairs.
+    # The site has generated a specific URL address for it,
+    # about which a message appears.
+
+    # User click on URL and see his to-do list.
+
+    # Say bye-bye to tomorrow.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
