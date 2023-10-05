191edc1d4b8ce9a3929776aad3042312c8772d09
Alyona Dudina <badyukiko@gmail.com>
2023-10-05 19:10:21 +0300

    refactor home_page module-test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve, reverse
 from lists.views import HomePageView
@@ -9,17 +10,7 @@ class HomePageTest(TestCase):
     def setUp(self) -> None:
         self.url = reverse('home')
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Test: root URL resolving to view of home_page."""
-        request = HttpRequest()
-        request.method = 'GET'
-        request.path = '/'
-        resolver = resolve(request.path)
-        self.assertEqual(resolver.func.view_class, HomePageView)
-
     def test_home_page_returns_correct_html(self):
-        """Test: home page return correct html."""
+        """Test: home page is using correct view and template"""
         response = self.client.get(self.url)
-        html = response.content.decode()
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTemplateUsed(response, 'lists/home.html')
