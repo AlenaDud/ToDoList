0cd3bcafaf5e43a88924f7dc38dec5d53ac64a02
Alyona Dudina <badyukiko@gmail.com>
2023-10-05 18:22:51 +0300

    fix module_testhome page URL and template

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import View
-from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.base import TemplateResponseMixin, View, TemplateView
 
 
-class HomePageView(TemplateResponseMixin, View):
+class HomePageView(View):
     """Return home page"""
-
+    template_name = 'lists/home.html'
 
     def get(self, request):
-        pass
+        return render(request, 'lists/home.html')
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'lists.apps.AppConfig',
+    'lists.apps.ListsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
